a82e572c1cabf4724eef4088e835444bae610d6e
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-03-09 19:12:39 -0600

    Created app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+  
+    //Start the Game
+    const startGame = () => {
+      const playBtn = document.querySelector(".intro button");
+      const introScreen = document.querySelector(".intro");
+      const match = document.querySelector(".match");
+  
+      playBtn.addEventListener("click", () => {
+        introScreen.classList.add("fadeOut");
+        match.classList.add("fadeIn");
+      });
+    };
+   
